0f7eb477d0c990cfdab3d6dd9bd0e040aacb93ee
zakariasemaoui44 <tvhomesemaoui@gmail.com>
2026-06-15 16:58:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def test():
     return {"yt_dlp_version": result.stdout.strip()}
 
 def get_yt_dlp_cmd(url: str, extra_args: list = []):
-    cmd = ["yt-dlp", "--no-playlist", "--js-runtimes", "node"]
+    cmd = ["yt-dlp", "--no-playlist"]
 
     if os.path.exists(COOKIES_PATH):
         cmd += ["--cookies", COOKIES_PATH]
